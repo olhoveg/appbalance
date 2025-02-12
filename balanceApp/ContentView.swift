d454baab61c7629e6d18f8e66f8959de88e9304e
Evgeniy <olhoveg@gmail.com>
2025-02-13 01:10:25 +0300

    Обновление главной страницы

--- balanceApp/ContentView.swift
+++ balanceApp/ContentView.swift
@@ -14,19 +14,55 @@ struct ContentView: View {
                     Image(systemName: "person.fill")
                     Text("Профиль")
                 }
+            
+            MapView()
+                .tabItem {
+                    Image(systemName: "map.fill")
+                    Text("Карта")
+                }
+            
+            ChatView()
+                .tabItem {
+                    Image(systemName: "bubble.left.and.bubble.right.fill")
+                    Text("Чат")
+                }
         }
     }
 }
 
+// Главная
 struct HomeView: View {
     var body: some View {
         Text("Главная страница")
+            .font(.title)
+            .padding()
     }
 }
 
+// Профиль
 struct ProfileView: View {
     var body: some View {
         Text("Профиль")
+            .font(.title)
+            .padding()
+    }
+}
+
+// Карта
+struct MapView: View {
+    var body: some View {
+        Text("Карта")
+            .font(.title)
+            .padding()
+    }
+}
+
+// Чат
+struct ChatView: View {
+    var body: some View {
+        Text("Чат")
+            .font(.title)
+            .padding()
     }
 }
 
